0160b91d4254001cc439617f232eaf79d83c5d5e
tjdgus903 <tjdgusgus123@gmail.com>
2025-08-10 17:04:09 +0900

    벌크연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -39,16 +39,19 @@ public class JpaMain {
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            // 전체 값을 셋팅하는 것이 벌크 연산
+            // 벌크연산은 영속성컨텍스트를 무시하고 db에 직접 쿼리
+            // 자동 flush 호출
+            // 벌크연산이 있으면 쿼리를 수행한 뒤 영속성컨텍스트 처리가 됨
+            // 벌크연산 사용 뒤에는 영속성 컨텍스트를 초기화 하자
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                            .executeUpdate();
+
             em.clear();
 
-            List<Member> resuiltList = em.createQuery("Member.findByUsername", Member.class)
-                            .setParameter("username", "회원1")
-                            .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resuiltList) {
-                System.out.println("list = "+member);
-            }
+            System.out.println("result = "+findMember);
 
             tx.commit();
         }catch(Exception e){
